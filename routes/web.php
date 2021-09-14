ece433548fe34e9fb63786bd9746649b16f815f4
socho009 <cs_boularak@esi.dz>
2021-09-14 15:02:47 +0100

    put and delete verbs corrections

--- routes/web.php
+++ routes/web.php
@@ -26,15 +26,15 @@ Route::group(['middleware'=>['auth'],'prefix'=>'user'],function(){
     Route::get('/post/add',[PostController::class,'add'])->name('post.add');
     Route::post('/post/save',[PostController::class,'store'])->name('post.store');
     Route::get('/post/view/{post}',[PostController::class,'view'])->name('post.view');
-    Route::post('/post/update',[PostController::class,'update'])->name('post.update');
-    Route::get('/post/delete/{post}',[PostController::class,'delete'])->name('post.delete');
+    Route::put('/post/update',[PostController::class,'update'])->name('post.update');
+    Route::delete('/post/delete/{post}',[PostController::class,'delete'])->name('post.delete');
 
     Route::get('/links',[LinkController::class,'index'])->name('link.list');
     Route::get('/link/add',[LinkController::class,'add'])->name('link.add');
     Route::post('/link/save',[LinkController::class,'store'])->name('link.store');
     Route::get('/link/view/{link}',[LinkController::class,'view'])->name('link.view');
-    Route::post('/link/update',[LinkController::class,'update'])->name('link.update');
-    Route::get('/link/delete/{link}',[LinkController::class,'delete'])->name('link.delete');
+    Route::put('/link/update',[LinkController::class,'update'])->name('link.update');
+    Route::delete('/link/delete/{link}',[LinkController::class,'delete'])->name('link.delete');
 });
 
 
